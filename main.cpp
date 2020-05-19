a2cd374847c0d27118ebfda1df3280e627d45b5a
Giovane Pereira <gytios123@gmail.com>
2020-05-19 08:34:27 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main()
 {
+    int QuantCompra;
+    double ValorCompra;
     cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
     cout << "--------------------------------" <<endl;
     cout << "INFORME OS DADOS DO ULTIMO ANO" <<endl;
     cout << endl;
-    cout << "Quantas compras o cliente fez no ultimo ano?"<<endl;
-    cout << "Qual o ticket medio?"<<endl;
+    cout << "Quantas compras o cliente fez no ultimo ano? ";
+    cin >> QuantCompra;
+    cout << "Qual o ticket medio? ";
+    cin >> ValorCompra;
     cout << endl;
     cout << "Quantas vezes o cliente atrasou o pagamento?"<<endl;
     cout << "A maioria das compras foi em dinheiro, cartao, ou boleto (D/C/B)?"<<endl;
